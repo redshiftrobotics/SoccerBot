b333ee8a7fc14beea860716538e5a596e47e4017
Ari Porad <ari@ariporad.com>
2018-10-21 10:16:30 -0700

    chore: remove speed scalar

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Soccerbot.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Soccerbot.kt
@@ -10,17 +10,14 @@ class Soccerbot(): OpMode() {
     private lateinit var leftDrive: DcMotor
     private lateinit var rightDrive: DcMotor
 
-    private var visitorSpeedScalar = 0.5
-    private val visitorSpeedScalarIncrement = 0.1
-
     private val override get() = gamepad2.left_trigger > 0.5 || gamepad2.right_trigger > 0.5
 
     private var lastLB = false
     private var lastRB = false
 
     override fun init() {
-        leftDrive = hardwareMap.dcMotor.get("r1m0")!!.also { it.direction = DcMotorSimple.Direction.FORWARD }
-        rightDrive = hardwareMap.dcMotor.get("r1m1")!!.also { it.direction = DcMotorSimple.Direction.REVERSE }
+        leftDrive = hardwareMap.dcMotor.get("left")!!.also { it.direction = DcMotorSimple.Direction.REVERSE }
+        rightDrive = hardwareMap.dcMotor.get("right")!!.also { it.direction = DcMotorSimple.Direction.FORWARD }
 
         telemetry.addLine("Soccerbot Initialized!")
         telemetry.addLine()
@@ -34,22 +31,17 @@ class Soccerbot(): OpMode() {
                 ---------------------------
                 !!!!!! OVERRIDE MODE !!!!!!
                 VISITOR DRIVING IS DISABLED
-                Visitor Speed: ${visitorSpeedScalar}x
-                (Use LB/RB to modify)
                 ---------------------------
             """.trimIndent())
             leftDrive.power = gamepad2.left_stick_y.toDouble()
             rightDrive.power = gamepad2.right_stick_y.toDouble()
 
-            if (gamepad2.left_bumper && !lastLB) visitorSpeedScalar -= visitorSpeedScalarIncrement
-            if (gamepad2.right_bumper && !lastRB) visitorSpeedScalar += visitorSpeedScalarIncrement
-
             lastLB = gamepad1.left_bumper
             lastRB = gamepad1.right_bumper
         } else {
             telemetry.addLine("Welcome to Seattle Academy!")
-            leftDrive.power = gamepad1.left_stick_y.toDouble() * visitorSpeedScalar
-            rightDrive.power = gamepad1.right_stick_y.toDouble() * visitorSpeedScalar
+            leftDrive.power = gamepad1.left_stick_y.toDouble()
+            rightDrive.power = gamepad1.right_stick_y.toDouble()
         }
 
         telemetry.update()
